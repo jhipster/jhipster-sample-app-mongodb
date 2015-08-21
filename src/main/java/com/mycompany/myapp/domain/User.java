17a9bd92f966a11cd77eba1726a15dc480fdf298
Julien Dubois <julien.dubois@gmail.com>
2015-08-21 10:26:40 +0200

    automatic project update

--- src/main/java/com/mycompany/myapp/domain/User.java
+++ src/main/java/com/mycompany/myapp/domain/User.java
@@ -198,7 +198,6 @@ public class User extends AbstractAuditingEntity implements Serializable {
     public String toString() {
         return "User{" +
                 "login='" + login + '\'' +
-                ", password='" + password + '\'' +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 ", email='" + email + '\'' +
