2122567cdaa0e752c2f0280a754e5a509888156b
Julien Dubois <julien.dubois@gmail.com>
2015-10-05 15:45:27 +0200

    automatic project update

--- src/main/java/com/mycompany/myapp/config/CacheConfiguration.java
+++ src/main/java/com/mycompany/myapp/config/CacheConfiguration.java
@@ -5,8 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 import org.springframework.cache.support.NoOpCacheManager; 
 
 import javax.annotation.PreDestroy;
