c2877126ecceb1f437eb862a7792df205e4b87e7
Julien Dubois <julien.dubois@gmail.com>
2015-12-30 11:15:44 +0100

    automatic project update

--- src/test/javascript/spec/app/entities/bankAccount/bankAccount-detail.controller.spec.js
+++ src/test/javascript/spec/app/entities/bankAccount/bankAccount-detail.controller.spec.js
@@ -1,38 +1,42 @@
 'use strict';
 
-describe('BankAccount Detail Controller', function() {
-    var $scope, $rootScope;
-    var MockEntity, MockBankAccount;
-    var createController;
+describe('Controller Tests', function() {
 
-    beforeEach(inject(function($injector) {
-        $rootScope = $injector.get('$rootScope');
-        $scope = $rootScope.$new();
-        MockEntity = jasmine.createSpy('MockEntity');
-        MockBankAccount = jasmine.createSpy('MockBankAccount');
-        
+    describe('BankAccount Detail Controller', function() {
+        var $scope, $rootScope;
+        var MockEntity, MockBankAccount;
+        var createController;
 
-        var locals = {
-            '$scope': $scope,
-            '$rootScope': $rootScope,
-            'entity': MockEntity ,
-            'BankAccount': MockBankAccount
-        };
-        createController = function() {
-            $injector.get('$controller')("BankAccountDetailController", locals);
-        };
-    }));
+        beforeEach(inject(function($injector) {
+            $rootScope = $injector.get('$rootScope');
+            $scope = $rootScope.$new();
+            MockEntity = jasmine.createSpy('MockEntity');
+            MockBankAccount = jasmine.createSpy('MockBankAccount');
+            
+
+            var locals = {
+                '$scope': $scope,
+                '$rootScope': $rootScope,
+                'entity': MockEntity ,
+                'BankAccount': MockBankAccount
+            };
+            createController = function() {
+                $injector.get('$controller')("BankAccountDetailController", locals);
+            };
+        }));
 
 
-    describe('Root Scope Listening', function() {
-        it('Unregisters root scope listener upon scope destruction', function() {
-            var eventType = 'sampleMongDBApp:bankAccountUpdate';
+        describe('Root Scope Listening', function() {
+            it('Unregisters root scope listener upon scope destruction', function() {
+                var eventType = 'sampleMongDBApp:bankAccountUpdate';
 
-            createController();
-            expect($rootScope.$$listenerCount[eventType]).toEqual(1);
+                createController();
+                expect($rootScope.$$listenerCount[eventType]).toEqual(1);
 
-            $scope.$destroy();
-            expect($rootScope.$$listenerCount[eventType]).toBeUndefined();
+                $scope.$destroy();
+                expect($rootScope.$$listenerCount[eventType]).toBeUndefined();
+            });
         });
     });
+
 });
