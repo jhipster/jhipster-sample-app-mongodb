c909f28173aa8d1e83a1a6ea0602135adf52d13e
Julien Dubois <julien.dubois@gmail.com>
2017-05-20 04:31:23 +0200

    automatic project update

--- src/main/java/io/github/jhipster/sample/repository/BankAccountRepository.java
+++ src/main/java/io/github/jhipster/sample/repository/BankAccountRepository.java
@@ -1,6 +1,7 @@
 package io.github.jhipster.sample.repository;
 
 import io.github.jhipster.sample.domain.BankAccount;
+import org.springframework.stereotype.Repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
@@ -8,6 +9,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
  * Spring Data MongoDB repository for the BankAccount entity.
  */
 @SuppressWarnings("unused")
+@Repository
 public interface BankAccountRepository extends MongoRepository<BankAccount,String> {
 
 }
--- src/main/java/io/github/jhipster/sample/service/AuditEventService.java
+++ src/main/java/io/github/jhipster/sample/service/AuditEventService.java
@@ -2,12 +2,12 @@ package io.github.jhipster.sample.service;
 
 import io.github.jhipster.sample.config.audit.AuditEventConverter;
 import io.github.jhipster.sample.repository.PersistenceAuditEventRepository;
-import java.time.LocalDateTime;
 import org.springframework.boot.actuate.audit.AuditEvent;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.Optional;
 
 /**
@@ -36,7 +36,7 @@ public class AuditEventService {
             .map(auditEventConverter::convertToAuditEvent);
     }
 
-    public Page<AuditEvent> findByDates(LocalDateTime fromDate, LocalDateTime toDate, Pageable pageable) {
+    public Page<AuditEvent> findByDates(Instant fromDate, Instant toDate, Pageable pageable) {
         return persistenceAuditEventRepository.findAllByAuditEventDateBetween(fromDate, toDate, pageable)
             .map(auditEventConverter::convertToAuditEvent);
     }
