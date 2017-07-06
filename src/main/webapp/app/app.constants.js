303df2ef8efb81286aaec804734feca3dfd57a34
Julien Dubois <julien.dubois@gmail.com>
2017-07-06 15:51:49 +0200

    automatic project update

--- src/main/webapp/app/app.constants.js
+++ src/main/webapp/app/app.constants.js
@@ -5,5 +5,6 @@
         .module('jhipsterMongodbSampleApplicationApp')
         .constant('VERSION', "0.0.1-SNAPSHOT")
         .constant('DEBUG_INFO_ENABLED', true)
+        .constant('BUILD_TIMESTAMP', "")
 ;
 })();
