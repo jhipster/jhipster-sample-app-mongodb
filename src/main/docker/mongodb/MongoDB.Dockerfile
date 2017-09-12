0f7844517b3c21f0fc26f2e825384e942f10ef11
Julien Dubois <julien.dubois@gmail.com>
2017-09-12 14:34:48 +0200

    automatic project update

--- src/main/docker/mongodb/MongoDB.Dockerfile
+++ src/main/docker/mongodb/MongoDB.Dockerfile
@@ -1,2 +1,2 @@
-FROM mongo:3.2.10
+FROM mongo:3.4.8
 ADD mongodb/scripts/init_replicaset.js init_replicaset.js
--- src/main/java/io/github/jhipster/sample/config/JacksonConfiguration.java
+++ src/main/java/io/github/jhipster/sample/config/JacksonConfiguration.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.zalando.problem.ProblemModule;
+import org.zalando.problem.validation.ConstraintViolationProblemModule;
 
 @Configuration
 public class JacksonConfiguration {
@@ -15,4 +17,21 @@ public class JacksonConfiguration {
     public AfterburnerModule afterburnerModule() {
         return new AfterburnerModule();
     }
+
+    /*
+     * Module for serialization/deserialization of RFC7807 Problem.
+     */
+    @Bean
+    ProblemModule problemModule() {
+        return new ProblemModule();
+    }
+
+    /*
+     * Module for serialization/deserialization of ConstraintViolationProblem.
+     */
+    @Bean
+    ConstraintViolationProblemModule constraintViolationProblemModule() {
+        return new ConstraintViolationProblemModule();
+    }
+
 }
