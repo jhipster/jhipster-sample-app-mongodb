366dba661ea4e9a759821647adad9e2bc77efcec
Julien Dubois <julien.dubois@gmail.com>
2018-10-03 16:22:16 +0200

    automatic project update

--- src/main/java/io/github/jhipster/sample/repository/BankAccountRepository.java
+++ src/main/java/io/github/jhipster/sample/repository/BankAccountRepository.java
@@ -1,8 +1,10 @@
 package io.github.jhipster.sample.repository;
 
 import io.github.jhipster.sample.domain.BankAccount;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+
 
 /**
  * Spring Data MongoDB repository for the BankAccount entity.
