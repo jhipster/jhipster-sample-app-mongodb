2d667b7f5a89b348509d3d54744e29d292fe0561
Pascal Grimaud <pascalgrimaud@gmail.com>
2020-06-28 12:24:25 +0200

    automatic project update

--- src/main/java/io/github/jhipster/sample/config/JacksonConfiguration.java
+++ src/main/java/io/github/jhipster/sample/config/JacksonConfiguration.java
@@ -29,7 +29,7 @@ public class JacksonConfiguration {
      * Module for serialization/deserialization of RFC7807 Problem.
      */
     @Bean
-    ProblemModule problemModule() {
+    public ProblemModule problemModule() {
         return new ProblemModule();
     }
 
@@ -37,7 +37,7 @@ public class JacksonConfiguration {
      * Module for serialization/deserialization of ConstraintViolationProblem.
      */
     @Bean
-    ConstraintViolationProblemModule constraintViolationProblemModule() {
+    public ConstraintViolationProblemModule constraintViolationProblemModule() {
         return new ConstraintViolationProblemModule();
     }
 }
