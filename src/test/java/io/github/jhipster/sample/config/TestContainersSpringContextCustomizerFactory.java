99f186d23376cb278ee9931979591eb645560ff8
Matt Raible <matt@raibledesigns.com>
2024-04-09 07:58:02 -0400

    automatic project update for v8.3.0

--- src/test/java/io/github/jhipster/sample/config/TestContainersSpringContextCustomizerFactory.java
+++ src/test/java/io/github/jhipster/sample/config/TestContainersSpringContextCustomizerFactory.java
@@ -5,10 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.test.context.ContextConfigurationAttributes;
 import org.springframework.test.context.ContextCustomizer;
 import org.springframework.test.context.ContextCustomizerFactory;
+import org.springframework.test.context.MergedContextConfiguration;
 
 public class TestContainersSpringContextCustomizerFactory implements ContextCustomizerFactory {
 
@@ -18,21 +20,34 @@ public class TestContainersSpringContextCustomizerFactory implements ContextCust
 
     @Override
     public ContextCustomizer createContextCustomizer(Class<?> testClass, List<ContextConfigurationAttributes> configAttributes) {
-        return (context, mergedConfig) -> {
-            ConfigurableListableBeanFactory beanFactory = context.getBeanFactory();
-            TestPropertyValues testValues = TestPropertyValues.empty();
-            EmbeddedMongo mongoAnnotation = AnnotatedElementUtils.findMergedAnnotation(testClass, EmbeddedMongo.class);
-            if (null != mongoAnnotation) {
-                log.debug("detected the EmbeddedMongo annotation on class {}", testClass.getName());
-                log.info("Warming up the mongo database");
-                if (null == mongoDbBean) {
-                    mongoDbBean = beanFactory.createBean(MongoDbTestContainer.class);
-                    beanFactory.registerSingleton(MongoDbTestContainer.class.getName(), mongoDbBean);
-                    // ((DefaultListableBeanFactory)beanFactory).registerDisposableBean(MongoDbTestContainer.class.getName(), mongoDbBean);
+        return new ContextCustomizer() {
+            @Override
+            public void customizeContext(ConfigurableApplicationContext context, MergedContextConfiguration mergedConfig) {
+                ConfigurableListableBeanFactory beanFactory = context.getBeanFactory();
+                TestPropertyValues testValues = TestPropertyValues.empty();
+                EmbeddedMongo mongoAnnotation = AnnotatedElementUtils.findMergedAnnotation(testClass, EmbeddedMongo.class);
+                if (null != mongoAnnotation) {
+                    log.debug("detected the EmbeddedMongo annotation on class {}", testClass.getName());
+                    log.info("Warming up the mongo database");
+                    if (null == mongoDbBean) {
+                        mongoDbBean = beanFactory.createBean(MongoDbTestContainer.class);
+                        beanFactory.registerSingleton(MongoDbTestContainer.class.getName(), mongoDbBean);
+                        // ((DefaultListableBeanFactory)beanFactory).registerDisposableBean(MongoDbTestContainer.class.getName(), mongoDbBean);
+                    }
+                    testValues = testValues.and("spring.data.mongodb.uri=" + mongoDbBean.getMongoDBContainer().getReplicaSetUrl());
                 }
-                testValues = testValues.and("spring.data.mongodb.uri=" + mongoDbBean.getMongoDBContainer().getReplicaSetUrl());
+                testValues.applyTo(context);
             }
-            testValues.applyTo(context);
+
+            @Override
+            public int hashCode() {
+                return MongoDbTestContainer.class.getName().hashCode();
+            }
+
+            @Override
+            public boolean equals(Object obj) {
+                return this.hashCode() == obj.hashCode();
+            }
         };
     }
 }
