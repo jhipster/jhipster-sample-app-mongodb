a8d6a5fffe8f30604e90377780a47f77e5ac8bb1
Matt Raible <matt@raibledesigns.com>
2024-06-24 17:48:29 -0600

    automatic project update for v8.6.0

--- src/main/java/io/github/jhipster/sample/security/DomainUserDetailsService.java
+++ src/main/java/io/github/jhipster/sample/security/DomainUserDetailsService.java
@@ -19,7 +19,7 @@ import org.springframework.stereotype.Component;
 @Component("userDetailsService")
 public class DomainUserDetailsService implements UserDetailsService {
 
-    private final Logger log = LoggerFactory.getLogger(DomainUserDetailsService.class);
+    private static final Logger log = LoggerFactory.getLogger(DomainUserDetailsService.class);
 
     private final UserRepository userRepository;
 
--- src/main/java/io/github/jhipster/sample/web/rest/AuthorityResource.java
+++ src/main/java/io/github/jhipster/sample/web/rest/AuthorityResource.java
@@ -24,7 +24,7 @@ import tech.jhipster.web.util.ResponseUtil;
 @RequestMapping("/api/authorities")
 public class AuthorityResource {
 
-    private final Logger log = LoggerFactory.getLogger(AuthorityResource.class);
+    private static final Logger log = LoggerFactory.getLogger(AuthorityResource.class);
 
     private static final String ENTITY_NAME = "adminAuthority";
 
